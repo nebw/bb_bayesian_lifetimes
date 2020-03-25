b6242eed666ab4f0546c1fd804a6867eb5f28ad8
Benjamin Wild <b.w@fu-berlin.de>
2020-03-25 09:49:06 +0100

    Use tag reuse table as rough prior for hatchdates
    For 2019 data.

--- bayesian_lifetimes/slurm.py
+++ bayesian_lifetimes/slurm.py
@@ -1,3 +1,5 @@
+import datetime
+
 import numpy as np
 import pandas as pd
 
@@ -46,11 +48,23 @@ def get_data_for_bee(
     return (bee_id, emerged_doy, died_doy, detections, trace)
 
 
-def generate_jobs(detections_path, log_detections_threshold=9.5, use_hatchdates=False, **kwargs):
+def datetime_to_doy(dt):
+    return (dt - datetime.datetime(year=dt.year, month=1, day=1, tzinfo=dt.tzinfo)).days + 1
+
+
+def generate_jobs(
+    detections_path, log_detections_threshold=9.5, use_hatchdates=False, p_hatchdates=None, **kwargs
+):
     detections = pd.read_parquet(detections_path)
+    m = meta.BeeMetaInfo()
+
+    if p_hatchdates == "bb19":
+        assert "min_doy" in kwargs and "max_doy" in kwargs
+        idmapping = m.idmapping
+        idmapping["doy"] = idmapping["date"].apply(datetime_to_doy)
+        idmapping.sort_values("doy", inplace=True)
 
     if use_hatchdates:
-        m = meta.BeeMetaInfo()
         valid_bees = m.hatchdates[np.logical_not(pd.isna(m.hatchdates.hatchdate))]
         bees = list(map(BeesbookID.from_dec_12, valid_bees.dec12))
         valid_ids = detections.bee_id.isin(set(map(lambda bee_id: bee_id.as_ferwar(), bees)))
@@ -61,6 +75,14 @@ def generate_jobs(detections_path, log_detections_threshold=9.5, use_hatchdates=
     detections = detections[detections.bee_id.isin(set(log_dets.index.values))]
 
     for bee_id, bee_detections in detections.groupby("bee_id"):
+        if p_hatchdates == "bb19":
+            earliest_doy = idmapping[idmapping.mapped_id == bee_id].doy.iloc[0]
+            tag_id = idmapping[idmapping.mapped_id == bee_id].bee_id.iloc[0]
+            later_reuses = idmapping[idmapping.bee_id == tag_id]
+            later_reuses = later_reuses[later_reuses.doy > earliest_doy]
+            latest_doy = later_reuses.doy.iloc[0] if len(later_reuses) > 0 else kwargs["max_doy"]
+            kwargs["p_hatchdates"] = np.arange(earliest_doy, latest_doy) - kwargs["min_doy"]
+
         yield dict(
             bee_id=bee_id, bee_detections=bee_detections, use_tagged_date=use_hatchdates, **kwargs
         )
